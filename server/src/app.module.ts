4a7088e5999f4611b908ef781902c0cdf59c41d7
jaycethanks <jaycethanks@outlook.com>
2024-08-14 17:06:15 +0800

    chore: configs about docker-compose and prisma

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, 
+  //   ConfigModule.forRoot({
+  //   envFilePath: ['../.env'],
+  //   // ignoreEnvFile: process.env.NODE_ENV === 'production', // 在生产环境中不加载 .env 文件
+  // })
+],
   controllers: [AppController],
   providers: [AppService],
 })

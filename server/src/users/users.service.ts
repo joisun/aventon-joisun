c1c2095f3d887ebd36eaa8c26eaa9b487d61bcac
jaycethanks <jaycethanks@outlook.com>
2024-08-15 17:49:42 +0800

    feat: optimize performance with Promise.all for user query by page

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -38,42 +38,46 @@ export class UsersService {
   async queryUsersByPage(data: QueryUserDto): Promise<QueryUsersByPage> {
     try {
       const { query, currentPage = 1, size = 5 } = data
+      const [count, result] = await Promise.all([
+        this.prisma.users.count({
+          where: {
+            OR: [
+              // https://www.prisma.io/docs/orm/prisma-client/queries/case-sensitivity#mysql-provider
+              // TODO: 如果想区分大小写怎么办？ - -
+              // { name: { contains: query, mode:'insensitive' } },
+              { name: { contains: query } },
+              { email: { contains: query } },
+              { address: { contains: query } },
+              { avatar: { contains: query } },
+              { f_color: { contains: query } },
+              { f_food: { contains: query } }
+            ],
+  
+          }
+        }),
+        this.prisma.users.findMany({
+        // https://www.prisma.io/docs/orm/prisma-client/queries/pagination#offset-pagination
+          skip: currentPage,// index
+          take: size,// pagesize
+          where: {
+            OR: [
+              { name: { contains: query } },
+              { email: { contains: query } },
+              { address: { contains: query } },
+              { avatar: { contains: query } },
+              { f_color: { contains: query } },
+              { f_food: { contains: query } }
+            ],
+          },
+          orderBy: {
+            date: 'desc',
+          },
+        })
 
-      const count = await this.prisma.users.count({
-        where: {
-          OR: [
-            // https://www.prisma.io/docs/orm/prisma-client/queries/case-sensitivity#mysql-provider
-            // TODO: 如果想区分大小写怎么办？ - -
-            // { name: { contains: query, mode:'insensitive' } },
-            { name: { contains: query } },
-            { email: { contains: query } },
-            { address: { contains: query } },
-            { avatar: { contains: query } },
-            { f_color: { contains: query } },
-            { f_food: { contains: query } }
-          ],
+      ])
 
-        }
-      });
-      const totalPages = Math.ceil(count) / size;
-      // https://www.prisma.io/docs/orm/prisma-client/queries/pagination#offset-pagination
-      const result = await this.prisma.users.findMany({
-        skip: currentPage,// index
-        take: size,// pagesize
-        where: {
-          OR: [
-            { name: { contains: query } },
-            { email: { contains: query } },
-            { address: { contains: query } },
-            { avatar: { contains: query } },
-            { f_color: { contains: query } },
-            { f_food: { contains: query } }
-          ],
-        },
-        orderBy: {
-          date: 'desc',
-        },
-      })
+      const totalPages = Math.ceil(count / size) ;
+
       return { data: result, total: totalPages } as QueryUsersByPage
     } catch (err) {
       console.log('err', err)
@@ -98,4 +102,4 @@ export class UsersService {
   // remove(id: number) {
   //   return `This action removes a #${id} user`;
   // }
-}
+

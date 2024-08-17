2cddc85f590e9bd6f9ad6db12b392acec7d3ed8c
jaycethanks <jaycethanks@outlook.com>
2024-08-18 00:03:55 +0800

    feat(web): support emptyholder

---
+++ web/src/components/ui/home-page/empty-placeholder.vue
@@ -0,0 +1,11 @@
+<script lang='ts' setup>
+import { EmptyBoxIcon } from '@/components/icons';
+</script>
+<template>
+    <div class="w-full flex flex-col justify-center items-center min-h-48 animate-fadeUp">
+        <EmptyBoxIcon class="text-7xl text-foreground-secondary"/>
+        <span class="text-sm text-foreground-secondary">No Matched Result</span>
+    </div>
+</template>
+<style lang='less' scoped>
+</style>

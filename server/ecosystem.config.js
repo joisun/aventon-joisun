9f36a5b7e37c243739fce38f4f86f5c831e51e41
jaycethanks <jaycethanks@outlook.com>
2024-08-17 14:55:34 +0800

    chore: add ecosystem.config.js for pm2 boot

---
+++ server/ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'joi-pm2-nest',
+      script: 'main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
